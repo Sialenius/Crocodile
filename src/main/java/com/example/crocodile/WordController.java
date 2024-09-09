86277aacc51e5a3516d1ba01d71ed575e53c09df
Aliaksei Sialenia <aliaksei.sialenia@gmail.com>
2024-09-09 17:02:24 +0300

    Made better function to get a random word

--- src/main/java/com/example/crocodile/WordController.java
+++ src/main/java/com/example/crocodile/WordController.java
@@ -3,15 +3,52 @@ package com.example.crocodile;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Random;
+//import java.io.BufferedReader;
+//import java.io.FileReader;
+//import java.util.ArrayList;
 
 @RestController
 public class WordController {
+    private static String greetings = "Привет, Крокодил! 🐊\n";
+    private static String[] words = {"Баскетбол", "Каяк", "Мотоцикл", "Кросфит"};
+    private String previousWord = null;
 
-    //String[] words = {Кальян, };
+  /*  WordController() {
+        BufferedReader reader = new BufferedReader(new FileReader("~/IdeaProjects/Crocodile/scr/main/resources/words.txt"));
+        String str;
+
+        ArrayList<String> list = new ArrayList<String>();
+        while((str = reader.readLine()) != null ){
+            if(!str.isEmpty()){
+                list.add(str);
+                System.out.println(str);
+            }}
+        words = list.toArray(new String[0]);
+    }
+
+
+   */
+
 
     @GetMapping("/word")
     public String getWord() {
-        //System.out.println("Привет, Крокодил!");
-        return "Привет, Крокодил! 🐊";
+
+        Random random = new Random();
+        int index = random.nextInt(words.length);
+        while (previousWord == words[index]) {
+            index = random.nextInt(words.length);
+        }
+        previousWord = words[index];
+
+
+
+        //return words[index];
+
+        String message = greetings + System.lineSeparator() + words[index];
+        return message;
     }
+
+
+
 }
